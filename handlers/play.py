95fb0c67c6b329b52cac366fbada5f70af8549f2
Elnur <104248864+elnur2232839@users.noreply.github.com>
2022-06-03 23:45:48 +0400

    update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -109,7 +109,7 @@ async def play(_, message: Message):
     try:
         user = await USER.get_me()
     except:
-        user.first_name = "Rahid_Asistan"
+        user.first_name = "LegendAsistant"
     usar = user
     wew = usar.id
     try:
@@ -198,8 +198,8 @@ async def play(_, message: Message):
             keyboard = InlineKeyboardMarkup(
         [
             [
-                InlineKeyboardButton("🚀 Support", url=f"https://t.me/Gencler_Mekani"),
-                InlineKeyboardButton("📣 Kanal", url=f"https://t.me/Rahid_44"),
+                InlineKeyboardButton("🚀 Support", url=f"https://t.me/SecretMMC"),
+                InlineKeyboardButton("📣 Kanal", url=f"https://t.me/SecretMMC"),
             ],[
                 InlineKeyboardButton("📲 Bağla", callback_data="cls"),
             ],
@@ -263,8 +263,8 @@ async def play(_, message: Message):
         keyboard = InlineKeyboardMarkup(
         [
             [
-                InlineKeyboardButton("🚀 Support", url=f"https://t.me/Gencler_Mekani"),
-                InlineKeyboardButton("📣 Kanal", url=f"https://t.me/Rahid_44"),
+                InlineKeyboardButton("🚀 Support", url=f"https://t.me/SecretMMC"),
+                InlineKeyboardButton("📣 Kanal", url=f"https://t.me/SecretMMC"),
             ],[
                 InlineKeyboardButton("📲 Bağla", callback_data="cls"),
             ],
